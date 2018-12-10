98ce454e0225e55d8bbe6176ce716d95cf2089c8
Steve N <nguy.steve@gmail.com>
2018-12-09 16:48:43 -0800

    deploy

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // // Main route 
 app.get('/', function (req, res) {
-      res.render('index')
+      res.render('/')
 })
 
 app.get('/all', function (req, res) {
